1bc0ee1c2cdd789cce3b11f7d98b8b5f8d8cc2d7
张榜 <bang.zhang@liulishuo.com>
2018-06-22 19:48:18 +0800

    does the same thing with the last commit to another different component

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,22 +1,42 @@
 // Basic unit test
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from '../login'
+import { on } from 'cluster';
+
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
   // create a fake object to hold the form field values (username and password)
+  const fakeUser = {
+    username: 'til',
+    password: 'til'
+  }
   // create a jest.fn() for your submit handler
+  const onSubmit = jest.fn()
   // render the Login component to a div
   // TIP: const div = document.createElement('div')
   //
+  const container = document.createElement('div')
+  ReactDOM.render(<Login onSubmit={onSubmit} />, container)
   // get the field nodes
+  const form = container.querySelector('form')
   // TIP: const inputs = div.querySelectorAll('input')
   // TIP: const form = div.querySelector('form')
   // fill in the field values
+  const { username, password } = form.elements
+  username.value = fakeUser.username
+  password.value = fakeUser.password
   //
   // Act
   // submit the form:
   // TIP: formNode.dispatchEvent(new window.Event('submit'))
   //
+  const submit = new window.Event('submit')
+  form.dispatchEvent(submit)
   // Assert
   // ensure your submit handler was called properly
+  expect(onSubmit).toHaveBeenCalledTimes(1)
+  expect(onSubmit).toHaveBeenCalledWith(fakeUser)
 })
 
 //////// Elaboration & Feedback /////////
